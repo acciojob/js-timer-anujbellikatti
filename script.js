47e22134776e47c7fbf3947be299eff6b6c7d440
anujbellikatti <134064947+anujbellikatti@users.noreply.github.com>
2023-10-21 17:32:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,8 +3,7 @@
 let timer = document.getElementById("timer");
 
 
-
-setInterval(function f() {
+function f() {
 let time = new Date();
 var day = time.getDate();
 var month = time.getMonth()+1;
@@ -14,7 +13,7 @@ var hour = time.getHours();
 var min = time.getMinutes();
 var sec = time.getSeconds();
 
-timer.innerText = day+"/"+month+"/"+year+" "+hour%12+":"+min+":"+sec;
-},1000);
+timer.innerText = day+"/"+month+"/"+year+","+hour%12+":"+min+":"+sec;
+};
 
 f();
